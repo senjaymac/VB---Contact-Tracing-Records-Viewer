50920b4c2d47ea485a788fc9130b01f06ead3a67
Senon Silvestre <42272961+senjaymac@users.noreply.github.com>
2022-07-07 19:27:50 +0800

    View Records Button Added
    View Records button Opens formRecordViewerTextFile(form) and hides the mainForm

---
+++ VB - Contact Tracing App/VB - Contact Tracing App/VB - Contact Tracing App/VB - Contact Tracing App/Form1.vb
@@ -0,0 +1,136 @@
+﻿Public Class formMain
+    Dim Gender, Q1, Q2, Q3, Q4 As String
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim form As System.IO.StreamWriter
+        form = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\Element 54\Documents\Repos\VB - Contact Tracing\VB - Contact Tracing App\Contact TracingForm.Txt", True)
+        form.WriteLine("")
+        form.WriteLine("Date: " + pickerDate.Text)
+        form.WriteLine("Time: " + DateTime.Now.ToString("h:mm:ss tt"))
+        form.WriteLine("")
+        form.WriteLine("Personal Informations")
+        form.WriteLine("First Name: " + txtbxFirstName.Text)
+        form.WriteLine("Last Name: " + txtbxLastName.Text)
+        form.WriteLine("Complete Address: " + txtbxAddress.Text)
+        form.WriteLine("Age: " + txtbxAge.Text)
+        form.WriteLine("Phone Number: " + txtbxPhoneNumber.Text)
+        form.WriteLine("Temperature: " + txtbxTemperature.Text)
+        form.WriteLine("Gender: " + Gender)
+        form.WriteLine("")
+        form.WriteLine("Health Questions")
+        form.WriteLine("Have you've been tested for COVID-19? " + Q1)
+        form.WriteLine("Are you experiencing any sickness recently? " + Q2)
+        form.WriteLine("Have you traveled outside of the country in the past 14days? " + Q3)
+        form.WriteLine("Do you have a close contact with a probable covid 19 person? " + Q4)
+        form.WriteLine("__________________________________________________________________________")
+        form.WriteLine("")
+        form.WriteLine("")
+        MessageBox.Show("Succesfully Submitted, Thank You!")
+        form.Close()
+
+
+    End Sub
+
+
+    Private Sub time(sender As Object, e As EventArgs) Handles timerClock.Tick
+
+        lblTimer.Text = DateTime.Now.ToLongTimeString()
+
+    End Sub
+
+    Private Sub txtbxNumber_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtbxPhoneNumber.KeyPress, txtbxAge.KeyPress, MyBase.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And Not e.KeyChar = Chr(Keys.Delete) And Not e.KeyChar = Chr(Keys.Back) And Not e.KeyChar = Chr(Keys.Space) Then
+            e.Handled = True
+            MessageBox.Show("Please Input Numbers Only on this Field!")
+
+        End If
+    End Sub
+
+    Private Sub txtbxAge_keypress(sender As Object, e As KeyPressEventArgs) Handles txtbxAge.KeyPress
+        If txtbxAge.Text.Length >= 2 Then
+            If e.KeyChar <> ControlChars.Back Then
+                e.Handled = True
+                MessageBox.Show("This field is up to 2 digits only")
+            End If
+        End If
+    End Sub
+
+    Private Sub rbYes4_CheckedChanged(sender As Object, e As EventArgs) Handles rbYes4.CheckedChanged
+        Q4 = "Yes"
+    End Sub
+
+    Private Sub rbYes3_CheckedChanged(sender As Object, e As EventArgs) Handles rbYes3.CheckedChanged
+        Q3 = "Yes"
+    End Sub
+
+    Private Sub rbYes2_CheckedChanged(sender As Object, e As EventArgs) Handles rbYes2.CheckedChanged
+        Q2 = "Yes"
+    End Sub
+
+    Private Sub rbYes1_CheckedChanged(sender As Object, e As EventArgs) Handles rbYes1.CheckedChanged
+        Q1 = "Yes"
+    End Sub
+
+    Private Sub rbNo1_CheckedChanged(sender As Object, e As EventArgs) Handles rbNo1.CheckedChanged
+        Q1 = "No"
+    End Sub
+
+    Private Sub rbNo3_CheckedChanged(sender As Object, e As EventArgs) Handles rbNo3.CheckedChanged
+        Q3 = "No"
+    End Sub
+
+    Private Sub rbNo2_CheckedChanged(sender As Object, e As EventArgs) Handles rbNo2.CheckedChanged
+        Q2 = "No"
+    End Sub
+
+    Private Sub rbMale_CheckedChanged(sender As Object, e As EventArgs) Handles rbMale.CheckedChanged
+        Gender = "Male"
+    End Sub
+
+    Private Sub btnViewRecords_Click(sender As Object, e As EventArgs) Handles btnViewRecords.Click
+        formRecordViewer.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub rbFemale_CheckedChanged(sender As Object, e As EventArgs) Handles rbFemale.CheckedChanged
+        Gender = "Female"
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtbxFirstName.Clear()
+        txtbxLastName.Clear()
+        txtbxAddress.Clear()
+        txtbxAge.Clear()
+        txtbxPhoneNumber.Clear()
+        txtbxTemperature.Clear()
+
+        rbMale.Checked = False
+        rbFemale.Checked = False
+
+        rbYes1.Checked = False
+        rbYes2.Checked = False
+        rbYes3.Checked = False
+        rbYes4.Checked = False
+
+        rbNo1.Checked = False
+        rbNo2.Checked = False
+        rbNo3.Checked = False
+        rbNo4.Checked = False
+
+    End Sub
+
+    Private Sub rbNo4_CheckedChanged(sender As Object, e As EventArgs) Handles rbNo4.CheckedChanged
+        Q4 = "No"
+    End Sub
+
+    Private Sub txtbxTemperature_keypress(sender As Object, e As KeyPressEventArgs) Handles txtbxTemperature.KeyPress
+        If txtbxTemperature.Text.Length >= 2 Then
+            If e.KeyChar <> ControlChars.Back Then
+                e.Handled = True
+                MessageBox.Show("This field is up to 2 digits only")
+            End If
+        End If
+    End Sub
+
+
+End Class
---
+++ VB - Contact Tracing App/VB - Contact Tracing App/VB - Contact Tracing App/VB - Contact Tracing App/Form2.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formRecordViewer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(formRecordViewer))
+        Me.txtbxRecordViewerTextFile = New System.Windows.Forms.TextBox()
+        Me.grpbxTitle = New System.Windows.Forms.GroupBox()
+        Me.lblPleaseFillUp = New System.Windows.Forms.Label()
+        Me.lblContactTraceForm = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.grpbxTitle.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtbxRecordViewerTextFile
+        '
+        Me.txtbxRecordViewerTextFile.Location = New System.Drawing.Point(12, 143)
+        Me.txtbxRecordViewerTextFile.Multiline = True
+        Me.txtbxRecordViewerTextFile.Name = "txtbxRecordViewerTextFile"
+        Me.txtbxRecordViewerTextFile.Size = New System.Drawing.Size(541, 290)
+        Me.txtbxRecordViewerTextFile.TabIndex = 0
+        '
+        'grpbxTitle
+        '
+        Me.grpbxTitle.BackColor = System.Drawing.Color.PaleGreen
+        Me.grpbxTitle.Controls.Add(Me.lblPleaseFillUp)
+        Me.grpbxTitle.Controls.Add(Me.lblContactTraceForm)
+        Me.grpbxTitle.Controls.Add(Me.PictureBox1)
+        Me.grpbxTitle.Location = New System.Drawing.Point(48, 32)
+        Me.grpbxTitle.Name = "grpbxTitle"
+        Me.grpbxTitle.Size = New System.Drawing.Size(483, 100)
+        Me.grpbxTitle.TabIndex = 3
+        Me.grpbxTitle.TabStop = False
+        '
+        'lblPleaseFillUp
+        '
+        Me.lblPleaseFillUp.AutoSize = True
+        Me.lblPleaseFillUp.Font = New System.Drawing.Font("Franklin Gothic Demi", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPleaseFillUp.ForeColor = System.Drawing.Color.Green
+        Me.lblPleaseFillUp.Location = New System.Drawing.Point(129, 63)
+        Me.lblPleaseFillUp.Name = "lblPleaseFillUp"
+        Me.lblPleaseFillUp.Size = New System.Drawing.Size(123, 21)
+        Me.lblPleaseFillUp.TabIndex = 4
+        Me.lblPleaseFillUp.Text = "Records Viewer"
+        '
+        'lblContactTraceForm
+        '
+        Me.lblContactTraceForm.AutoSize = True
+        Me.lblContactTraceForm.Font = New System.Drawing.Font("Franklin Gothic Demi", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContactTraceForm.Location = New System.Drawing.Point(29, 16)
+        Me.lblContactTraceForm.Name = "lblContactTraceForm"
+        Me.lblContactTraceForm.Size = New System.Drawing.Size(319, 37)
+        Me.lblContactTraceForm.TabIndex = 3
+        Me.lblContactTraceForm.Text = "Contact Tracing Form"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(366, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(117, 100)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'formRecordViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGreen
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(565, 567)
+        Me.Controls.Add(Me.grpbxTitle)
+        Me.Controls.Add(Me.txtbxRecordViewerTextFile)
+        Me.Name = "formRecordViewer"
+        Me.ShowIcon = False
+        Me.Text = "Records Viewer"
+        Me.grpbxTitle.ResumeLayout(False)
+        Me.grpbxTitle.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtbxRecordViewerTextFile As TextBox
+    Friend WithEvents grpbxTitle As GroupBox
+    Friend WithEvents lblPleaseFillUp As Label
+    Friend WithEvents lblContactTraceForm As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
